3446dc888c3045a89f1fe46381b6e8afb0e2bdf6
Roberto-Nsoni <roberto.lulenga.nsoni@estudiantat.upc.edu>
2025-12-14 11:54:03 +0100

    ''

--- app/tests/test_fluencia.py
+++ app/tests/test_fluencia.py
@@ -74,7 +74,7 @@ TEST_DURATION = 60
 # -----------------------------
 # Configuración OpenAI
 # -----------------------------
-API_KEY = "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"
+API_KEY = "API_KEY"
 client = openai.OpenAI(api_key=API_KEY)
 
 # -----------------------------
