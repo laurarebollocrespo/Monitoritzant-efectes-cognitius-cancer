e0376d32b2bf29e501a5c96fdedbe943e3ea3613
Roberto-Nsoni <roberto.lulenga.nsoni@estudiantat.upc.edu>
2025-12-14 11:26:19 +0100

    ''

---
+++ OncoConnect/app/MainScreen.tsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <WebView
+        source={{ uri: 'http://10.66.51.224:8501' }} // aquí la URL de tu Streamlit
+        style={{ flex: 1 }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
